33fb0798fe75a1bb0d68addde3738318d323978a
terrycojones <terry@jon.es>
2017-09-18 15:51:14 +0200

    Added count-fasta.py

--- count-fasta.py
+++ count-fasta.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+
+"""
+This script counts the number of sequences in a FASTA file given
+on standard input. The result is printed to standard output.
+"""
+
 import sys
 
 count = 0
